0a6ec8513cf9f392a6ac1531bdc6f8b6f89b412e
Jason Dagit <dagitj@gmail.com>
2019-01-16 10:37:23 -0800

    Squashed commit of the following:
    Refactors and cleanups:
    
      * Add a new module, list.lemmas, for additional lemmas on lists.
      * Simplifies `trunc` to use addition in the type instead of `min`
      * Adds new functions:
        * bsf, index of least-significant 1 bit
        * bsr, index of most-significant 1 bit
        * split_at, splits a bit vector at an index
        * chunks, splits a bitvector into smaller chunks (note: the bit
        order may not be what you expect)
        * concat, joins a list of bitvectors together
        * chunks_nat, splits a nat into a list of bitvectors, order has the
        most siginificant bit at the head of the list.
        * chunks', which is chunks_nat lifted to bitvector
        * concat_nat, like concat but returns a nat.
      * Several new lemmas for the definitions mentioned above
      * New convenience lemmas for natural numbers.

---
+++ lean/list/lemmas.lean
@@ -0,0 +1,63 @@
+/-
+Copyright (c) 2018 Galois. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+
+Additional lemmas for natural numbers.
+
+This is a work-in-progress, and contains additions to other theories.
+-/
+
+import tactic.find
+import ..tactic
+
+open nat
+
+section reverse
+  universes u
+
+  lemma reverse_core_step {α : Type u} (x : α) (xs : list α) (ys : list α)
+  : list.reverse_core (x::ys) xs = list.reverse_core ys [] ++ x::xs :=
+  begin
+    induction ys with y ys ih generalizing x xs,
+    case list.nil
+    { simp [list.reverse_core], },
+    case list.cons
+    { rw [ih],
+      simp [list.reverse_core],
+      rw [←ih],
+      simp [list.reverse_core],
+    },
+  end
+
+  lemma reverse_length_nil {α : Type u} (xs : list α)
+  : list.length (list.reverse_core xs list.nil) = list.length xs :=
+  begin
+    induction xs with x xs ih,
+    case list.nil
+    { simp [list.reverse_core], },
+    case list.cons
+    { simp [reverse_core_step],
+      rw ih,
+    },
+  end
+
+  @[simp]
+  lemma length_reverse {α : Type u} {x : list α} : list.length (list.reverse x) = list.length x :=
+    begin
+      simp [list.reverse],
+      apply reverse_length_nil,
+    end
+
+  lemma reverse_step (α : Type u) (x : α) (ys : list α)
+  : list.reverse (x :: ys) = list.reverse ys ++ [x] :=
+  begin
+    induction ys with y ys ih,
+    case list.nil
+    { simp [list.reverse, list.reverse_core] },
+    case list.cons
+    { simp [list.reverse],
+      rw reverse_core_step,
+    },
+  end
+
+end reverse
