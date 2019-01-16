6b3cfc5a2f0a5bc540e8be759b1e47f8b6219430
Jason Dagit <dagitj@gmail.com>
2019-01-16 10:45:35 -0800

    list.lemmas: Fix a typo in a comment

--- lean/list/lemmas.lean
+++ lean/list/lemmas.lean
@@ -2,7 +2,7 @@
 Copyright (c) 2018 Galois. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 
-Additional lemmas for natural numbers.
+Additional lemmas for lists.
 
 This is a work-in-progress, and contains additions to other theories.
 -/
