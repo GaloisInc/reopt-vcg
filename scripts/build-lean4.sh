a3ba7fc738b9ef54016f1b2133f8577246c45450
Simon Winwood <simonjwinwood@gmail.com>
2019-07-30 13:37:16 -0700

    Updated travis

---
+++ scripts/build-lean4.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+set -e
+
+if [ $# < 3 ];
+then
+    echo "Usage: build-lean4.sh srcdir builddir installdir"
+    exit 1
+fi   
+
+SRCDIR=$(cd $1 && pwd)
+RELBUILDDIR=$2
+mkdir -p $3
+INSTALLDIR=$(cd $3 && pwd)
+
+echo rm -rf $RELBUILDDIR
+mkdir -p $RELBUILDDIR
+BUILDDIR=$(cd $RELBUILDDIR && pwd)
+
+# GITREV=$(cd $SRCDIR && git rev-parse --short HEAD)
+
+pushd $BUILDDIR > /dev/null
+
+cmake  $SRCDIR/src -G Ninja -DCMAKE_BUILD_TYPE=debug -DCMAKE_INSTALL_PREFIX=$INSTALLDIR             
+ninja
+ninja install
+popd
+# ninja install
+# rm -f $HOME/opt/lean4
+# ln -s $HOME/opt/lean4-$GITREV/ $HOME/opt/lean4
