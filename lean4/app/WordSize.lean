abcefeee9667d159e37d0635cf4b491bbf869f79
Andrew Kent <andrew@galois.com>
2020-06-18 20:39:16 -0700

    initial prover interface functions

---
+++ lean4/app/WordSize.lean
@@ -0,0 +1,32 @@
+
+import SMTLIB.Syntax
+
+
+inductive WordSize
+| word8  : WordSize
+| word16 : WordSize
+| word32 : WordSize
+| word64 : WordSize
+
+namespace WordSize
+
+def bytes : WordSize → Nat
+| word8  => 1
+| word16 => 2
+| word32 => 4
+| word64 => 8
+
+
+/-- Bit width (i.e., the nominal width) --/
+def bits (w:WordSize) : Nat := 8 * w.bytes
+
+def sort (w:WordSize) : SMT.sort := SMT.sort.bitvec w.bits
+
+def fromNat : Nat → Option WordSize
+| 8  => some word8
+| 16 => some word16
+| 32 => some word32
+| 64 => some word64
+| _  => none
+
+end WordSize
