dfd3d563453157e5cb701d2fc4e8955fd2b65c49
Simon Winwood <simonjwinwood@gmail.com>
2020-08-13 23:08:06 -0700

    Getting lean to build

--- .github/ci.sh
+++ .github/ci.sh
@@ -14,7 +14,10 @@ shift
 
 case $COMMAND in
     lean4-sha) git submodule status lean4/deps/lean4 | awk '{print "::set-output name=lean4-sha::"$1}' ;;
-    lean4-build) echo "build lean" ;;
+    #    lean4-build) ./scripts/build-lean4.sh lean4/deps/lean4/   ;;
+    lean4-build)
+        ./scripts/build-lean4.sh lean4/deps/lean4 lean4/deps/lean4/build opt/lean llvm-config-8 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
+        ;;
     build) echo "compiling";;
     *) echo "unknown command"; exit 1;;
 esac    
