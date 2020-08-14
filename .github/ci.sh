4b281d6b39d355d81ef9e350172450ebacd98b0b
Simon Winwood <simonjwinwood@gmail.com>
2020-08-14 14:29:00 -0700

    Enabled tests

--- .github/ci.sh
+++ .github/ci.sh
@@ -18,8 +18,12 @@ case $COMMAND in
         ./scripts/build-lean4.sh lean4/deps/lean4 lean4/deps/lean4/build opt/lean llvm-config-8 -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
         ;;
     build)
-        make --version
-        make -C lean4 LEAN_CXX=clang-8 LEAN=$PWD/opt/lean/bin/lean LEANC=$PWD/opt/lean/bin/leanc LLVM_CONFIG=llvm-config-8 CXX=clang-8
+        make -C lean4 LEAN_CXX=clang-8 LEAN=$PWD/opt/lean/bin/lean LEANC=$PWD/opt/lean/bin/leanc LLVM_CONFIG=llvm-config-8 CXX=clang-8 -j4
+        ;;
+    run-tests)
+        pushd lean4/tests/src/Test/ > /dev/null
+        bash test.sh
+        popd > /dev/null
         ;;
     *) echo "unknown command"; exit 1;;
 esac    
