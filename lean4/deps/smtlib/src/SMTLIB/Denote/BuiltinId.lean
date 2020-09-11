63a1f6034ee04b4637809fe4a20a9955f02154c0
Andrew Kent <andrew@galois.com>
2020-09-11 11:14:40 -0700

    merge bitvec code

--- lean4/deps/smtlib/src/SMTLIB/Denote/BuiltinId.lean
+++ lean4/deps/smtlib/src/SMTLIB/Denote/BuiltinId.lean
@@ -4,6 +4,7 @@ Denotations for SMT builtin functions.
 Copyright (c) 2020 Galois Inc. All rights reserved.
 -/
 
+import Galois.Data.Bitvec
 import SmtLib.Denote.SmtSort
 import SmtLib.Syntax
 
@@ -14,10 +15,10 @@ section
 variables {α β : Type} [DecidableLessOrder α] [DecidableLessOrder β]
 
 
-private def bvEqRangeAux {n} (a1 a2 : Array (BitVec n) β) (low : BitVec n) : Nat → Bool
+private def bvEqRangeAux {n} (a1 a2 : Array (bitvec n) β) (low : bitvec n) : Nat → Bool
 | Nat.zero => a1.select 0 == a2.select 0
 | Nat.succ i =>
-  let idx := low + (BitVec.ofNat n i) + 1;
+  let idx := low + (bitvec.of_nat n i) + 1;
   a1.select idx == a2.select idx && bvEqRangeAux i
 
 end
@@ -26,11 +27,11 @@ section
 variables {β : Type} [DecidableLessOrder β]
 
 
-def bvEqRange {n} (a1 a2 : Array (BitVec n) β) (low high : BitVec n) : Bool :=
+def bvEqRange {n} (a1 a2 : Array (bitvec n) β) (low high : bitvec n) : Bool :=
 if high < low then true
 else
   let rangeSize := high - low;
-  bvEqRangeAux a1 a2 low rangeSize.toNat
+  bvEqRangeAux a1 a2 low rangeSize.to_nat
 
 
 def eqRange :
@@ -99,50 +100,50 @@ protected def BuiltinIdent.denote : forall cs, BuiltinIdent cs → cs.denote
 | _, BuiltinIdent.store  k v           => Array.store
 | _, BuiltinIdent.eqrange k _          => Array.eqRange k
 
--- -- * BitVecs
+-- -- * bitvecs
 -- -- hex/binary literals
-| _, BuiltinIdent.concat _ _           => BitVec.append
-| _, BuiltinIdent.extract n i j        => BitVec.extract i j
+| _, BuiltinIdent.concat _ _           => bitvec.append
+| _, BuiltinIdent.extract n i j        => bitvec.extract i j
 -- -- unops
-| _, BuiltinIdent.bvnot   _            => BitVec.not
-| _, BuiltinIdent.bvneg   _            => BitVec.neg
+| _, BuiltinIdent.bvnot   _            => bitvec.not
+| _, BuiltinIdent.bvneg   _            => bitvec.neg
 -- -- binops                   
-| _, BuiltinIdent.bvand   _            => BitVec.and
-| _, BuiltinIdent.bvor    _            => BitVec.or
-| _, BuiltinIdent.bvadd   _            => BitVec.add
-| _, BuiltinIdent.bvmul   _            => BitVec.mul
-| _, BuiltinIdent.bvudiv  _            => BitVec.udiv
-| _, BuiltinIdent.bvurem  _            => BitVec.urem
-| _, BuiltinIdent.bvshl   _            => λ b i => BitVec.shl b i.toNat
-| _, BuiltinIdent.bvlshr  _            => λ b i => BitVec.ushr b i.toNat
+| _, BuiltinIdent.bvand   _            => bitvec.and
+| _, BuiltinIdent.bvor    _            => bitvec.or
+| _, BuiltinIdent.bvadd   _            => bitvec.add
+| _, BuiltinIdent.bvmul   _            => bitvec.mul
+| _, BuiltinIdent.bvudiv  _            => bitvec.udiv
+| _, BuiltinIdent.bvurem  _            => bitvec.urem
+| _, BuiltinIdent.bvshl   _            => λ b i => bitvec.shl b i.to_nat
+| _, BuiltinIdent.bvlshr  _            => λ b i => bitvec.ushr b i.to_nat
 -- -- comparison               
-| _, BuiltinIdent.bvult   _            => λ b1 b2 => decide (BitVec.ult b1 b2)
+| _, BuiltinIdent.bvult   _            => λ b1 b2 => decide (bitvec.ult b1 b2)
 
-| _, BuiltinIdent.bvnand  _            => λ b1 b2 => BitVec.not (BitVec.and b1 b2)
-| _, BuiltinIdent.bvnor   _            => λ b1 b2 => BitVec.not (BitVec.or b1 b2)
-| _, BuiltinIdent.bvxor   _            => BitVec.xor
-| _, BuiltinIdent.bvxnor  _            => λ b1 b2 => BitVec.not (BitVec.xor b1 b2)
+| _, BuiltinIdent.bvnand  _            => λ b1 b2 => bitvec.not (bitvec.and b1 b2)
+| _, BuiltinIdent.bvnor   _            => λ b1 b2 => bitvec.not (bitvec.or b1 b2)
+| _, BuiltinIdent.bvxor   _            => bitvec.xor
+| _, BuiltinIdent.bvxnor  _            => λ b1 b2 => bitvec.not (bitvec.xor b1 b2)
 | _, BuiltinIdent.bvcomp  _            => λ b1 b2 => if b1 = b2 then 1 else 0
-| _, BuiltinIdent.bvsub   _            => BitVec.sub
-| _, BuiltinIdent.bvsdiv  _            => BitVec.sdiv
-| _, BuiltinIdent.bvsrem  _            => BitVec.srem
-| _, BuiltinIdent.bvsmod  _            => BitVec.smod
-| _, BuiltinIdent.bvashr  _            => λ b i => BitVec.sshr b i.toNat
+| _, BuiltinIdent.bvsub   _            => bitvec.sub
+| _, BuiltinIdent.bvsdiv  _            => bitvec.sdiv
+| _, BuiltinIdent.bvsrem  _            => bitvec.srem
+| _, BuiltinIdent.bvsmod  _            => bitvec.smod
+| _, BuiltinIdent.bvashr  _            => λ b i => bitvec.sshr b i.to_nat
 
-| _, BuiltinIdent.repeat i _           => λ b => BitVec.repeat b i
+| _, BuiltinIdent.repeat i _           => λ b => bitvec.repeat b i
 
 | _, BuiltinIdent.zeroExtend  i n     => λ b => b.uresize (n + i)
 | _, BuiltinIdent.signExtend  i n     => λ b => b.sresize (n + i)
-| _, BuiltinIdent.rotateLeft  i _     => BitVec.rotateLeft i
-| _, BuiltinIdent.rotateRight i _     => BitVec.rotateRight i
+| _, BuiltinIdent.rotateLeft  i _     => bitvec.rotateLeft i
+| _, BuiltinIdent.rotateRight i _     => bitvec.rotateRight i
 
-| _, BuiltinIdent.bvule _              => λ x y => decide (BitVec.ule x y)
-| _, BuiltinIdent.bvugt _              => λ x y => decide (BitVec.ugt x y)
-| _, BuiltinIdent.bvuge _              => λ x y => decide (BitVec.uge x y)
-| _, BuiltinIdent.bvslt _              => λ x y => decide (BitVec.slt x y)
-| _, BuiltinIdent.bvsle _              => λ x y => decide (BitVec.sle x y)
-| _, BuiltinIdent.bvsgt _              => λ x y => decide (BitVec.sgt x y)
-| _, BuiltinIdent.bvsge _              => λ x y => decide (BitVec.sge x y)
+| _, BuiltinIdent.bvule _              => λ x y => decide (bitvec.ule x y)
+| _, BuiltinIdent.bvugt _              => λ x y => decide (bitvec.ugt x y)
+| _, BuiltinIdent.bvuge _              => λ x y => decide (bitvec.uge x y)
+| _, BuiltinIdent.bvslt _              => λ x y => decide (bitvec.slt x y)
+| _, BuiltinIdent.bvsle _              => λ x y => decide (bitvec.sle x y)
+| _, BuiltinIdent.bvsgt _              => λ x y => decide (bitvec.sgt x y)
+| _, BuiltinIdent.bvsge _              => λ x y => decide (bitvec.sge x y)
 
 
 end Raw
